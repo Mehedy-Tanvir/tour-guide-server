5afe8358d70537b6382222a4b485e37a9bc1547e
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-11-06 14:19:30 +0600

    server set up and added to github

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Tour guide server running");
+});
+// console.log(process.env.DB_USER, process.env.DB_PASSWORD);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.vnja0wm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  // Send a ping to confirm a successful connection
+  try {
+    // Get the database and collection on which to run the operation
+
+    const database = client.db("tourServicesDB");
+
+    const productsCollection = database.collection("products");
+    const bookingsCollection = database.collection("bookings");
+
+    // db ping
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+    // console.log("db not connecting");
+  }
+}
+run().catch(console.dir);
+app.listen(port, (req, res) => {
+  console.log(`Listening at port ${port}`);
+});
