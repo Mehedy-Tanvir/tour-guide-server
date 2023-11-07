452a02423dde0948374d5e21897fbeaa776d3ddd
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-11-07 11:27:23 +0600

    single service data fetching api created

--- index.js
+++ index.js
@@ -84,6 +84,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/serviceDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
+    });
+
     // db ping
     await client.db("admin").command({ ping: 1 });
     console.log(
